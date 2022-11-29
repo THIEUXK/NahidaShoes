1243a00c06da0cfb2f821ad2c8a91bbd782f1b74
CuongDeveloper <cuongvudevv@gmail.com>
2022-11-29 22:59:53 +0700

    VuLanhCuong

--- C_GUI/Program.cs
+++ C_GUI/Program.cs
@@ -4,11 +4,7 @@ namespace C_GUI
 {
     internal static class Program
     {
-        public static TrangChu TrangChu = new();
-<<<<<<< HEAD
-
-=======
->>>>>>> parent of 784c824 (Merge pull request #44 from CuongDeveloper/PH26084)
+        public static Login Login = new();
         /// <summary>
         ///  The main entry point for the application.
         /// </summary>
@@ -19,11 +15,7 @@ namespace C_GUI
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-<<<<<<< HEAD
-            Application.Run(new TrangChu());
-=======
-            Application.Run(TrangChu);
->>>>>>> parent of 784c824 (Merge pull request #44 from CuongDeveloper/PH26084)
+            Application.Run(Login);
         }
     }
 }

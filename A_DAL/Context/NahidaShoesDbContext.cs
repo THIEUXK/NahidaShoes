85727b1fee0157b1530a983581fe7293d5e02abf
cuongvulanh <vucuongdeveloper@gmail.com>
2022-12-08 23:35:47 +0700

    VuLanhCuong

--- A_DAL/Context/NahidaShoesDbContext.cs
+++ A_DAL/Context/NahidaShoesDbContext.cs
@@ -10,7 +10,7 @@ namespace A_DAL.Context
             base.OnConfiguring(optionsBuilder);
             //Đổi connectionString để connect với co sở dữ liệu của mình
 
-            _ = optionsBuilder.UseSqlServer("Data Source=DESKTOP-1813CVQ\\SQLEXPRESS;Initial Catalog=NahidaShos;User ID=Ph20260;Password=123456;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+            _ = optionsBuilder.UseSqlServer("Data Source=localhost,1433;Initial Catalog=NahidaShos;User ID=SA;Password=cuong191023;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
         }
         protected override void OnModelCreating(ModelBuilder builder)
         {
